7ccaf0727c24f8ef6c37e0dbce0a99ae329ea64f
Elena <elenam23@mail.ru>
2022-11-27 18:30:27 +0500

    Task 4

---
+++ Task4/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Clear();
+Console.Write("Введите 1-е число: ");
+int a = 
+Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите 2-е число ");
+int b = 
+Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите 3-е число ");
+int c = 
+Convert.ToInt32(Console.ReadLine());
+
+// int max = 0;
+// if ((a > b)&&(a > c))
+// max = a;
+// else if ((b > a)&&(b > c))
+// max = b;
+// if ((c > a)&&(c > b))
+// max = c;
+// Console.WriteLine(max);
+
+int max = new[] { a, b, c }.Max();
+Console.WriteLine(max);
