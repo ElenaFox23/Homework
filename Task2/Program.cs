a4d0a9e818f749773a97cfa0e67b7eb3a83e61ef
Elena <elenam23@mail.ru>
2022-11-27 17:20:49 +0500

    Task 2

---
+++ Task2/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+Console.Write("Введите 1-е число: ");
+int a = 
+Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите 2-е число ");
+int b = 
+Convert.ToInt32(Console.ReadLine());
+if (a > b)
+{ 
+ Console.WriteLine($"Максимальное число {a}");
+ Console.WriteLine($"Минимальное число {b}");
+}
+else
+{ 
+Console.WriteLine($"Максимальное число {b}");
+Console.WriteLine($"Минимальное число {a}");
+}    
