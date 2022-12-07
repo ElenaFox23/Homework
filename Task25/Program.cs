225af27420b0ea3406993c230bf17897f20e15cc
Elena <elenam23@mail.ru>
2022-12-07 20:01:15 +0500

    Возведение числа А в натур-ую степень В

---
+++ Task25/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите степень: ");
+int b = Convert.ToInt32(Console.ReadLine());
+int n = a;
+for (int i = 1; i < b; i++)
+n = n * a;
+Console.WriteLine($"{a} в степени {b} = " + n);
