75c2cf77796bf6a3f7c435024461014991be288d
Elena <elenam23@mail.ru>
2022-12-07 20:13:53 +0500

    Ьассив из 8 элементов, запрашивает и выводит на экран

---
+++ Task29/Program.cs
@@ -0,0 +1,10 @@
+﻿
+Console.Clear();
+int[]a = new int[8];
+Console.Write("Введите массив чисел ");
+for(int i = 0; i < 8; i++)
+{
+    a[i] = Convert.ToInt32(Console.ReadLine());
+}
+for(int i = 0; i < 8; i++)
+Console.Write($"{a[i]} " );
